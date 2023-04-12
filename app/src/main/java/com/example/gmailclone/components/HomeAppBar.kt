2eaa27e90b32752f137095eba48a09cb7f33e322
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-04-12 11:45:13 +0900

    AppBar修正

--- app/src/main/java/com/example/gmailclone/components/HomeAppBar.kt
+++ app/src/main/java/com/example/gmailclone/components/HomeAppBar.kt
@@ -1,11 +1,19 @@
 package com.example.gmailclone.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gmailclone.GmailApp
@@ -24,6 +32,16 @@ fun HomeAppBar() {
                     .fillMaxSize()
                     .padding(8.dp)
             ) {
+                Icon(Icons.Default.Menu, "Menu")
+                Text(text = "Search in gmail")
+
+                Image(
+                    painter = painterResource(id = com.example.gmailclone.R.drawable.ic_android_black_24dp),
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clip(CircleShape)
+                )
             }
         }
     }
